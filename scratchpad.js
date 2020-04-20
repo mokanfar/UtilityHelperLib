3921bf0c03d6523fe96a5fd82289eb22a076bd53
mokanfar <dannyz1984@gmail.com>
2020-04-19 17:42:45 -0700

    adfkfkf

--- scratchpad.js
+++ scratchpad.js
@@ -1,6 +1,84 @@
-const { Arr, Obj } = require("./javascript-prototypes.js");
-let a = Arr([1, 3, false, "", "", "", 3, 5]);
-let d = "a";
-let e = 2;
-let b = Obj({ "2": 1, b: 2 });
-console.log(e.isakeyof(b));
+const { Arr, Obj, ArrayHashMap } = require("./lib/javascript-prototypes.js");
+g = Arr([
+  2,
+  2,
+  5,
+  6,
+  2,
+  1,
+  6,
+  7,
+  8,
+  9,
+  3,
+  2,
+  1,
+  1,
+  1,
+  1,
+  1,
+  1,
+  1,
+  3,
+  4,
+  2,
+  23,
+  4,
+  5,
+  111,
+  2,
+  1,
+  1,
+]);
+s = Arr([
+  2,
+  6,
+  7,
+  2,
+  1,
+  1,
+  3,
+  2,
+  2,
+  2,
+  2,
+  2,
+  3,
+  5,
+  6,
+  7,
+  8,
+  9,
+  9,
+  9,
+  999,
+  9,
+  9,
+  9,
+  5,
+  2,
+  34,
+  11,
+]);
+
+let counter = 0;
+s.forEach((v) => {
+  if (v.isinarray(g)) {
+    counter = counter + 1;
+    g.sub(v);
+    s.sub(v);
+  }
+});
+
+s.removegarbage().srt();
+g.removegarbage().srt();
+s = Arr(s.filter((x) => x > g[0]));
+
+g.forEach((greed) => {
+  s = Arr(s.filter((x) => x > greed));
+  if (s.length > 0) {
+    counter++;
+    s.splice(0, 1, "");
+  }
+});
+counter;
--- test.coffee
+++ test.coffee
@@ -1,7 +1,8 @@
-{ Arr, Obj } = require  "./javascript-prototypes.js"
-c = console.log
-as = "loveleetcode".split('')
-a = Arr [1,2,3,4,5,6,0,0]
-idx = 0
+{ Zray, Zob, Arr, Obj } = require  "./javascript-prototypes.js"
 
-c idx.countnumberofoccurencesinarr(a)
+b = new Zray(1,2,34,5,6)
+iii = 34
+lll = new Zob({a:2,b:3})
+
+console.log iii
+
