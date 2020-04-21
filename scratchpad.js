bb1e963641c30f9142d9a3a9590675e74ffe7ecb
mokanfar <dannyz1984@gmail.com>
2020-04-21 01:25:21 -0700

    32235k

--- scratchpad.js
+++ scratchpad.js
@@ -1,1 +1,26 @@
-const { arr1, arr2, obj1, obj2, Arr, Obj, ArrayHashMap } = require("./lib/javascript-prototypes.js");
+const {
+  arr1,
+  arr2,
+  obj1,
+  obj2,
+  Arr,
+  Obj,
+  ArrayHashMap,
+} = require("./lib/javascript-prototypes.js");
+
+b = "abcdddeeeeaabbbcd";
+counter = [];
+letterchange = null;
+
+let a = Arr(b.split(""));
+ArrayHashMap(b);
+a;
+
+a.forEach((v, i) => {
+  if (a[i - 1] == a[i] && a[i + 1] == a[i]) {
+    counter.push([v, i - 1]);
+  }
+  if (a[i] !== a[i + 1]) letterchange = i;
+});
+
+counter;
